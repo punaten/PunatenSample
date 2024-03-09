e14f097fe7701483159efc160cfab1ab4206a2b7
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2024-03-09 10:47:28 +0900

    ランダム表示はできた

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -35,6 +35,13 @@ export default function Index() {
             Remix Docs
           </a>
         </li>
+        <li>
+          <a
+            href="/dog"
+          >
+            Dog
+          </a>
+        </li>
       </ul>
     </div>
   );
---
+++ app/routes/dog.tsx
@@ -0,0 +1,30 @@
+import { Link, json, useLoaderData } from '@remix-run/react';
+interface DogLoaderData {
+    message: string;
+    status: string;
+}
+function Dog() {
+    const data = useLoaderData<typeof loader>() as DogLoaderData;
+    return (
+        <div>
+            <img src={data.message} alt="random dog" />
+            <h1>{data.message}</h1>
+            <p>{data.status}</p>
+            <Link to="/">Go back</Link>
+        </div>
+    );
+}
+
+export default Dog;
+
+export async function loader({ request }: { request: Request }) {
+    // Implement your data loading logic here
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    return json(data);
+}
+
+export function action() {
+    // Implement your action logic here
+    // This function is optional and can be removed if not needed
+}
