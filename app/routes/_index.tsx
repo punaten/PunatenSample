6beb9de9e76f6afb87923400196fe502089b2335
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2024-03-09 16:21:38 +0900

    犬名検索ができるように

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -37,7 +37,7 @@ export default function Index() {
         </li>
         <li>
           <a
-            href="/dog"
+            href="/random-dog"
           >
             Dog
           </a>
---
+++ app/routes/dog.$breed.tsx
@@ -0,0 +1,45 @@
+import { LoaderFunctionArgs } from '@remix-run/cloudflare';
+import { Link, json, useLoaderData } from '@remix-run/react';
+interface DogLoaderData {
+    message: string;
+    status: string;
+}
+function Dog() {
+    const data = useLoaderData<typeof loader>() as DogLoaderData;
+
+    return (
+        <div>
+            <img src={data.message} alt="random dog" />
+            <h1>{data.message}</h1>
+            <p>{data.status}</p>
+            <Link to="/">Go back</Link>
+        </div>
+    );
+}
+
+export default Dog;
+
+export async function loader({
+    params,
+    request,
+}: LoaderFunctionArgs) {
+    // Implement your data loading logic herea
+    const cookie = request.headers.get("Cookie");
+
+    // parse the search params for `?q=`
+    const url = new URL(request.url);
+    const query = url.searchParams.get("q");
+
+    console.log(cookie, query, url)
+
+    const bread = params.breed;
+
+    const response = await fetch(`https://dog.ceo/api/breed/${bread}/images/random`);
+    const data = await response.json();
+    return json(data);
+}
+
+export function action() {
+    // Implement your action logic here
+    // This function is optional and can be removed if not needed
+}
